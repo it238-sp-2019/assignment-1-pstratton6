1b7ade1d45670e6eb0715b95231dead807d15bfb
pstratton6 <49204541+pstratton6@users.noreply.github.com>
2019-04-04 15:15:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('The new file');
+var x='a string variable';
+console.log(x);
+console.log(document);
+console.log($(document));
+console.log($('body'));
+console.log($);
+$( document ).ready(function() {
+console.log('I am ready');
+});
